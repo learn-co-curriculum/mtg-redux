d457f84fa43ca274684b0b5a3605f9651cdad112
toneloke <david.guerrero87@live.com>
2017-07-07 12:18:35 -0400

    https for mtg call

--- src/CardFactory.js
+++ src/CardFactory.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 const CardFactory = BaseComponent => {
   return class CardComponent extends Component {
     componentWillMount () {
-      const Url = 'http://api.magicthegathering.io/v1/cards/'
+      const Url = 'https://api.magicthegathering.io/v1/cards/'
       const cardId = this.props.match.params.cardId
       const apiUrl = cardId ? Url + cardId : Url
       fetch(apiUrl)
